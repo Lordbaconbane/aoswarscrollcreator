b154d5e100330f3c2f82d789e15979ddf5f3e7c0
Lordbaconbane <wesleychartrand@gmail.com>
2024-07-30 20:06:22 -0400

    Added border around legal

--- aoswarscrollcreator/src/components/Legal.tsx
+++ aoswarscrollcreator/src/components/Legal.tsx
@@ -8,13 +8,20 @@ const Legal: React.FC = () => {
         flexGrow: 0,
         p: 2,
         marginTop: 1,
-        width: 0.5,
+        width: 1,
       }}
     >
-      <Typography variant="body1" component="div" fontFamily={"Minion Pro"}>
+      <Typography
+        variant="body1"
+        component="div"
+        alignItems="center"
+        p={2}
+        bgcolor={"#201c1c"}
+        fontFamily={"Minion Pro"}
+        sx={{ border: "2px solid grey" }}
+      >
         {`
-        This website is a fan-made, UNOFFICIAL project. 
-        It is not affiliated with, endorsed, sponsored, 
+        This website is a fan-made, UNOFFICIAL project. It is not affiliated with, endorsed, sponsored, 
         or approved by Games Workshop. All content on 
         this site is intended for non-commercial, 
         no-charge digital distribution only.
