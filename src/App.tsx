d6471b317a737e553e80603dac5d52659796510a
Lordbaconbane <wesleychartrand@gmail.com>
2024-08-11 00:00:35 -0400

    Refreshing or closing page saves the state

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import { Box } from "@mui/material";
 
 import "../public/fonts/Minion Pro Regular.ttf";
+import { PersistGate } from "redux-persist/integration/react";
 import WarscrollCard from "./components/WarscrollCard/WarscrollCard";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
 import AccordianLayout from "./components/AccordianLayout";
 
@@ -23,17 +24,19 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline>
         <Provider store={store}>
-          <ResponsiveDrawer />
-          <Box sx={{ flexGrow: 1, width: "100%" }}>
-            <Grid container spacing={1}>
-              <Grid xs={12} md={6}>
-                <AccordianLayout />
+          <PersistGate loading={null} persistor={persistor}>
+            <ResponsiveDrawer />
+            <Box sx={{ flexGrow: 1, width: "100%" }}>
+              <Grid container spacing={1}>
+                <Grid xs={12} md={6}>
+                  <AccordianLayout />
+                </Grid>
+                <Grid xs={12} md={6}>
+                  <WarscrollCard />
+                </Grid>
               </Grid>
-              <Grid xs={12} md={6}>
-                <WarscrollCard />
-              </Grid>
-            </Grid>
-          </Box>
+            </Box>
+          </PersistGate>
         </Provider>
       </CssBaseline>
     </ThemeProvider>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,43 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { abilitiesSlice } from "../components/Abilities/AbilitiesSlice";
 import { characteristicSlice } from "../components/Characteristics/CharacteristicsSlice";
-import { configureStore } from "@reduxjs/toolkit";
 import { factionSlice } from "../components/GrandAlliances/GrandAlliancsSlice";
 import { keywordsSlice } from "../components/Keywords/KeywordsSlice";
 import { loadoutSlice } from "../components/Loadouts/LoadoutSlice";
 import { warscrollCardSlice } from "../components/WarscrollCard/WarscrollCardSlice";
 import { weaponsSlice } from "../components/Weapons/WeaponsSlice";
 
-// We created a Redux store
-export const store = configureStore({
-  reducer: {
-    faction: factionSlice.reducer,
-    warscroll: warscrollCardSlice.reducer,
-    characteristics: characteristicSlice.reducer,
-    keywords: keywordsSlice.reducer,
-    weapons: weaponsSlice.reducer,
-    abilities: abilitiesSlice.reducer,
-    loadout: loadoutSlice.reducer,
-  },
+// Combine reducers
+const rootReducer = combineReducers({
+  faction: factionSlice.reducer,
+  warscroll: warscrollCardSlice.reducer,
+  characteristics: characteristicSlice.reducer,
+  keywords: keywordsSlice.reducer,
+  weapons: weaponsSlice.reducer,
+  abilities: abilitiesSlice.reducer,
+  loadout: loadoutSlice.reducer,
 });
+
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Persistor
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
